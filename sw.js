4534de43f6a253347559f3831c97dcd9c231a445
ogpoopdogg <77356240+ogpoopdogg@users.noreply.github.com>
2026-09-08 15:52:53 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'ec-dispatch-v1';
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
